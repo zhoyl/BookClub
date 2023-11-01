0b39705a3578c9d3ba6912613a27b5325f52cb6d
КУПК ИТ <kupc_visual@outlook.com>
2023-11-01 13:28:44 +0500

    Добавьте файлы проекта.

---
+++ BookClub/BookClubOrder.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookClub
+{
+    /// <summary>
+    /// Логика взаимодействия для BookClubOrder.xaml
+    /// </summary>
+    public partial class BookClubOrder : Window
+    {
+      
+        //private Product _temp = new Product();
+        public BookClubOrder()
+        {
+            InitializeComponent();
+            //if (selectedProduct != null) { _temp = selectedProduct; }
+            //DataContext = _temp;
+        }
+    }
+}
---
+++ BookClub/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookClub
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        int kol = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dgTov.ItemsSource = bookClubEntities.GetContext().Product.ToList();
+        }
+
+        private void cmToTemp_Click(object sender, RoutedEventArgs e)
+        {
+            kol++;
+            lblKol.Content = kol.ToString();
+            btnTemp.Visibility = Visibility.Visible;
+            btnTemp.IsEnabled = true;
+           
+        }
+
+        private void btnTemp_Click(object sender, RoutedEventArgs e)
+        {
+            
+            BookClubOrder bko = new BookClubOrder();
+            bko.Show();
+            this.Close();
+
+        }
+    }
+}
